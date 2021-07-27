bb6c33419a16d8b1d6f9dd2edfff05f9223bec33
probably-not-porter <pelibby16@earlham.edu>
2021-07-27 19:58:35 -0400

    ssl

--- index.js
+++ index.js
@@ -19,7 +19,15 @@ app.get('/about', function(req, res) {
 
 
 
-// LISTEN
-app.listen(process.env.PORT || 8080, function(){
-    console.log("Server: Running on port %d in %s mode", this.address().port, app.settings.env);
-});
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
+
+https.createServer(options, function (req, res) {
+  res.writeHead(200);
+  res.end("hello world\n");
+}).listen(8000);
