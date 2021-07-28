0824288dd41c0edd066909ef7970a60470e143d0
probably-not-porter <pelibby16@earlham.edu>
2021-07-27 23:34:53 -0400

    update

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 var fs = require('fs');
 var http = require('http');
 var https = require('https');
-var privateKey  = fs.readFileSync('server.key', 'utf8');
-var certificate = fs.readFileSync('server.crt', 'utf8');
+var privateKey  = fs.readFileSync('host.key', 'utf8');
+var certificate = fs.readFileSync('host.crt', 'utf8');
 
 var credentials = {key: privateKey, cert: certificate};
 var express = require('express');
 var app = express();
-
-var privateKey = fs.readFileSync('sslcert/server.key');
-var certificate = fs.readFileSync('sslcert/server.crt');
-
-var credentials = {key: privateKey, cert: certificate};
-var app = express.createServer(credentials);
 
 
 app.use(express.static(__dirname + '/public'));
