d087760e475e7bd7dc6f9096f18437226fe915fb
probably-not-porter <pelibby16@earlham.edu>
2021-07-28 00:00:11 -0400

    http version

---
+++ index-http.js
@@ -0,0 +1,34 @@
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+//var privateKey  = fs.readFileSync('host.key', 'utf8');
+//var certificate = fs.readFileSync('host.cert', 'utf8');
+
+//var credentials = {key: privateKey, cert: certificate};
+var express = require('express');
+var app = express();
+
+
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+app.get('/', function(req, res) {
+    res.render("home",{
+    })
+});
+
+app.get('/', function(req, res) {
+    res.render("home",{
+    })
+});
+
+app.get('/about', function(req, res) {
+    res.render("about",{
+    })
+});
+
+
+var httpServer = http.createServer(app);
+v//ar httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(8080);
+//httpsServer.listen(8443);
